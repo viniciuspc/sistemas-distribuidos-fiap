04145d8b964ea838bd024d76df315423f5be2862
pasqualini9@gmail.com <pasqualini9@gmail.com@d505364d-1f93-60b8-1c26-c1ee368a0a8d>
2012-03-26 14:29:51 +0000

    Iniciar desenvolvimento de Thread

---
+++ Chat/src/br/com/fiap/scan/ThreadScan.java
@@ -0,0 +1,26 @@
+package br.com.fiap.scan;
+
+public class ThreadScan implements Runnable {
+
+	int portaInicial;
+	int portaFinal;
+	
+	
+	
+	public ThreadScan(int portaInicial, int portaFinal) {
+		super();
+		this.portaInicial = portaInicial;
+		this.portaFinal = portaFinal;
+	}
+
+
+
+	@Override
+	public void run() {
+		
+		
+		
+		
+	}
+
+}
